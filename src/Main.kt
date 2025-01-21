bd2c24d9e652fec33bd1c100767f42b4f3334e26
Caramelo <cccarlos@iesrafaelalberti.es>
2025-01-21 19:08:56 +0100

    Actualizacion

--- src/Main.kt
+++ src/Main.kt
@@ -1,14 +1,45 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-fun main() {
-    val name = "Kotlin"
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    println("Hello, " + name + "!")
+class Rectangulo(val base : Double, val altura : Double){
+    private var a : Double = 0.0
+    private var p : Double = 0.0
 
-    for (i in 1..5) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        println("i = $i")
+    init {
+        require(base > 0){"La base debe ser mayor que 0"}
+        require(altura > 0){"La altura debe ser mayor que 0"}
     }
+
+    fun area(): Double{
+        val a : Double = base * altura
+        return a
+    }
+
+    fun perimetro(): Double {
+        val p : Double= 2*base + 2*altura
+        return p
+    }
+
+    override fun toString(): String {
+        return "El rectangulo de base $base y altura $altura tiene un area de $a y perimetro de $p"
+    }
+}
+
+fun main(){
+
+    val rectangulo1 = Rectangulo(8.0,9.0)
+    rectangulo1.area()
+    rectangulo1.perimetro()
+    println(rectangulo1)
+
+    try {
+        val rectangulo2 = Rectangulo(-8.0,9.0)
+        rectangulo2.area()
+        rectangulo2.perimetro()
+        println(rectangulo2)
+    }catch (e: IllegalArgumentException){"ERROR: ${e.message}"}
+
+    try {
+        val rectangulo3 = Rectangulo(8.0,-9.0)
+        rectangulo3.area()
+        rectangulo3.perimetro()
+        println(rectangulo3)
+    }catch (e: IllegalArgumentException){"ERROR: ${e.message}"}
 }
